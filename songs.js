84a15f54f1ab275d968936131207ecc539a0fdc5
ManoKau <141646776+ManoKau@users.noreply.github.com>
2023-09-25 10:04:10 -0400

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,54 @@
+export default [
+    {
+      
+      src: "./music/comeRecover.mp3",
+      name: "Come Recover - Son Lux",
+      image: "./img/sonLux.jpg"
+    },
+    {
+      src: "./music/friends.mp3",
+      name: "Are We Still Friends? - Tyler The Creator",
+      image: "./img/tyler.jpg"
+      
+    },
+    {
+      src: "./music/interestelar.mp3",
+      name: "Cornfield Chase - Hans Zimmer",
+      image: "./img/interestelar.jpg"
+    },
+    {
+      src: "./music/jogosVorazes.mp3",
+      name: "The Hanging Tree - James Newton Howard",
+      image: "./img/jogosVorazes.jpg"
+    },
+    {
+      src: "./music/kiss.mp3",
+      name: "Kiss Me More Censored - Doja Cat",
+      image: "./img/kiss.jpg"
+    },
+    {
+      src: "./music/letGo.mp3",
+      name: "Let Go - Ark Patrol, Veronika Redd",
+      image: "./img/letGo.jpg"
+    },
+    {
+      src: "./music/macDemarco.mp3",
+      name: "Freaking Out the Neighborhood - Mac Demarco",
+      image: "./img/demarco.jpg"
+    },
+    {
+      src: "./music/musicBrasileira.mp3",
+      name: "Onde Anda Você - Tiago Nacarato",
+      image: "./img/brasil.jpg"
+    },
+    {
+      src: "./music/oppenheimer.mp3",
+      name: "Can You Hear The Music - Ludwig Göransson",
+      image: "./img/oppenheimer.jpg"
+    },
+    {
+      src: "./music/stevenUniverse.mp3",
+      name: "Peace and Love - Rebecca Sugar",
+      image: "./img/stevenUniverse.jpg"
+    },
+  ];
